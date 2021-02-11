3f0f8b45099673a274d8e782032fc235b2ec36db
SJ Lee <nocte55is@gmail.com>
2021-02-11 01:13:00 +0000

    Add chapter 4 from NDB

---
+++ calc_exec_time.py
@@ -0,0 +1,16 @@
+import timeit
+# ======================================== #
+
+# Input code here
+
+# ======================================== #
+start_time = timeit.default_timer()
+# ======================================== #
+
+# Your code here
+
+# ======================================== #
+end_time = timeit.default_timer()
+exec_time = end_time - start_time
+print('Total execution time: %fs' % exec_time)
+# ======================================== #
---
+++ main.py
@@ -0,0 +1,22 @@
+# p.118 구현 - 게임 개발
+import timeit
+# ======================================== #
+
+
+# ======================================== #
+start_time = timeit.default_timer()
+# ======================================== #
+
+
+
+# ======================================== #
+end_time = timeit.default_timer()
+exec_time = end_time - start_time
+print('Total execution time: %fs' % exec_time)
+# ======================================== #
+"""
+Test cases
+
+
+
+"""
