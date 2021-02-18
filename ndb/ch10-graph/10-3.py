d0ecd8339bd775c08aa08cb6659b01cf85aabe65
SJ Lee <nocte55is@gmail.com>
2021-02-18 05:29:57 +0000

    Update codes from chapter 10

--- ndb/ch10-graph/10-3.py
+++ ndb/ch10-graph/10-3.py
@@ -1,3 +1,19 @@
+# p.300 그래프 이론 - 도시 분할 계획
+"""
+7 12
+1 2 3
+1 3 2
+3 2 1
+2 5 2
+3 4 4
+7 3 6
+5 1 5
+1 6 2
+6 4 1
+6 5 3
+4 5 3
+6 7 4
+"""
 import sys
 input = sys.stdin.readline
 
@@ -17,34 +33,33 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 
-# V: number of vertices
-# E: number of edges
 V, E = map(int, input().split())
-
-# Initialize parent table
 parent = [0] * (V + 1)
-
-# edges: edge list
-# result: Total cost
 edges = []
-result = 0
+ans = 0
 
-# Initialize parent of each node as themselves
 for i in range(1, V + 1):
     parent[i] = i
 
-# Get edges information as input
 for _ in range(E):
     a, b, cost = map(int, input().split())
     edges.append((cost, a, b))
 
 edges.sort()
 
+# max_cost_mst: 'MST에 포함되는' 간선 중 최대 유지 비용을 갖는 간선의 비용
+max_cost_mst = 0
+
+# MST 구성
 for edge in edges:
     cost, a, b = edge
-    # Union only if adding the edge does not create a cycle
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
-        result += cost
+        ans += cost
 
-print(result)
+        # 'MST에 포함되는' 간선 중 최대 유지 비용을 갖는 간선을 찾음
+        max_cost_mst = cost
+
+# 두 개의 MST로 분리
+ans -= max_cost_mst
+print(ans)
