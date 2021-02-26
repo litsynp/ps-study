c92ac1d748a4dca94e39e8f5c8d2d92f5f682040
SJ Lee <nocte55is@gmail.com>
2021-02-26 15:20:35 +0000

    Memorize count sort

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
-def quick_sort(array, start, end):
-    if start >= end:
-        return
+import sys
+input = sys.stdin.readline
 
-    pivot = start
-    left = start + 1
-    right = end
-
-    while left <= right:
-        while left <= end and array[left] <= array[pivot]:
-            left += 1
-        while right > start and array[right] >= array[pivot]:
-            right -= 1
-        if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[left], array[right] = array[right], array[left]
-
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
+array = list(map(int, input().split()))
 
 
-def quick_sort_v2(array):
-    if len(array) <= 1:
-        return array
+def count_sort(array):
+    count = [0] * (max(array) + 1)
 
-    pivot = array[0]
-    tail = array[1:]
+    for i in range(len(array)):
+        count[array[i]] += 1
 
-    left_side = [x for x in tail if x <= pivot]
-    right_side = [x for x in tail if x > pivot]
-
-    return left_side + [pivot] + right_side
-
-
-array = [5, 7, 9, 0, 3, 1, 2, 4, 8]
-quick_sort(array, 0, len(array) - 1)
+    for i in range(len(count)):
+        for j in range(count[i]):
+            print(i, end=' ')
