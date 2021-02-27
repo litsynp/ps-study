7deb7ae27414e5da7769f05d847c91a36feb483e
SJ Lee <nocte55is@gmail.com>
2021-02-27 13:28:06 +0000

    Memorize binary search algorithm

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import sys
 input = sys.stdin.readline
 
+
+def binary_search_recursive(array, target, start, end):
+    if start > end:
+        return
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search_recursive(array, target, start, mid - 1)
+    else:
+        return binary_search_recursive(array, target, mid + 1, end)
+
+
+def binary_search_iterative(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+n, target = list(map(int, input().split()))
 array = list(map(int, input().split()))
 
+result = binary_search_recursive(array, target, 0, n - 1)
+print(result + 1 if result is not None else '원소가 존재하지 않습니다')
 
-def count_sort(array):
-    count = [0] * (max(array) + 1)
-
-    for i in range(len(array)):
-        count[array[i]] += 1
-
-    for i in range(len(count)):
-        for j in range(count[i]):
-            print(i, end=' ')
+result = binary_search_iterative(array, target, 0, n - 1)
+print(result + 1 if result is not None else '원소가 존재하지 않습니다')
