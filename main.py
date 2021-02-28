6f03154086915be6fa28f185bb0df1dc3016e88a
SJ Lee <nocte55is@gmail.com>
2021-02-28 15:49:58 +0000

    Memorize Dijkstra algorithm with priority queue

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
+import heapq
 import sys
 input = sys.stdin.readline
+INF = int(1e9)
+
+V, E = map(int, input().split())
+start = int(input())
+graph = [[] for _ in range(V + 1)]
+distance = [INF] * (V + 1)
 
 
-def binary_search_recursive(array, target, start, end):
-    if start > end:
-        return
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search_recursive(array, target, start, mid - 1)
-    else:
-        return binary_search_recursive(array, target, mid + 1, end)
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
 
-def binary_search_iterative(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
+dijkstra(start)
 
-
-n, target = list(map(int, input().split()))
-array = list(map(int, input().split()))
-
-result = binary_search_recursive(array, target, 0, n - 1)
-print(result + 1 if result is not None else '원소가 존재하지 않습니다')
-
-result = binary_search_iterative(array, target, 0, n - 1)
-print(result + 1 if result is not None else '원소가 존재하지 않습니다')
+for i in graph:
+    print(distance[i] if distance[i] != INF else 'INFINITY')
