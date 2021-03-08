43bb0239eed288dd0a251d4c9207c47a16bf1fab
SJ Lee <nocte55is@gmail.com>
2021-03-08 14:23:04 +0000

    Memorize Floyd-Warshall algorithm

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
 V, E = map(int, input().split())
-start = int(input())
-graph = [[] for _ in range(V + 1)]
-distance = [INF] * (V + 1)
+graph = [[INF] * (V + 1) for _ in range(V + 1)]
 
+for a in range(1, V + 1):
+    for b in range(1, V + 1):
+        if a == b:
+            graph[a][b] = 0
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+for i in range(E):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
 
-    while q:
-        dist, now = heapq.heappop(q)
+for k in range(1, V + 1):
+    for a in range(1, V + 1):
+        for b in range(1, V + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-
-dijkstra(start)
-
-for i in graph:
-    print(distance[i] if distance[i] != INF else 'INFINITY')
+for a in range(1, V + 1):
+    for b in range(1, V + 1):
+        print(graph[a][b] if graph[a][b] != INF else 'INFINITY', end=' ')
+    print()
