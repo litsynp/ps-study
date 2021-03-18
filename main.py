96de8193cbb81abccf995278c4239909c6282be1
SJ Lee <nocte55is@gmail.com>
2021-03-18 15:16:22 +0000

    Memorize quick sort from NDB

--- main.py
+++ main.py
@@ -2,35 +2,19 @@ import sys
 input = sys.stdin.readline
 
 
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    tail = arr[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-
-    if a < b:
-        b = parent[a]
-    else:
-        a = parent[b]
-
-
-V, E = map(int, input().split())
-parent = [i for i in range(V + 1)]
-
-for i in range(E):
-    a, b = map(int, input().split())
-    union_parent(parent, a, b)
-
-print('각 원소가 속한 집합: ', end='')
-for i in range(1, V + 1):
-    print(find_parent(parent, i), end=' ')
-
-print()
-
-print('부모 집합: ', end='')
-for i in range(1, V + 1):
-    print(parent[i], end=' ')
+arr = list(map(int, input().split()))
+arr = quick_sort(arr)
+print(arr)
